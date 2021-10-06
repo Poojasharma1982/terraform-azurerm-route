97f1ee863fb1bc178af5b1fbc50620e912aca399
Poojasharma1982 <89822894+Poojasharma1982@users.noreply.github.com>
2021-10-05 23:29:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,7 @@
-
+resource "azurerm_route" "myroute-vnetlocal" {
+  name                = var.route-name
+  resource_group_name = var.resource-group-name
+  route_table_name    = var.route-table-name
+  address_prefix      = "10.1.0.0/16"
+  next_hop_type       = "vnetlocal"
+}
